80d3efb3f119873549d2c3ac37197662db2d76f5
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2020-11-14 13:49:56 +0300

    (m) You can specify several types, including undefined.

--- src/using_decorator/main.py
+++ src/using_decorator/main.py
@@ -1,6 +1,17 @@
 """
-Checking that arguments belong to certain types.
+Checking that arguments belong to certain types
+You can specify several types,
+    including undefined which means that the argument was not be passed into a function and it has default value
+
+You can yse it like that:
+def function(*, kwarg1: str, kwarg2: types([dict, type(None), undefined])):
+    pass
+-----
+
 Checking that an argument belongs to a certain subset of possible variants.
+
+-----
+
 Implementation using a decorator.
 """
 
@@ -9,17 +20,31 @@ from doctest import testmod
 
 
 # --------------------------------------------------
+class types(list):
+    pass
+
+
+class undefined(object):
+    pass
+# --------------------------------------------------
+
+
+# --------------------------------------------------
 def validate_type_argument(func):
 
     def wrapper(*args, **kwargs):
-        # print('func.__annotations__', func.__annotations__)
-        for name_argument, type_argument in func.__annotations__.items():
-            if not isinstance(kwargs[name_argument], type_argument):
+        # print('\nfunc.__annotations__', func.__annotations__)
+        for name_argument, types_argument in func.__annotations__.items():
+            if type(types_argument) is not types:
+                types_argument = types([types_argument])
+            if (name_argument not in kwargs) and (undefined in types_argument):
+                continue
+            if type(kwargs[name_argument]) not in types_argument:
                 raise TypeError("'Argument '%s' for function %s%s must be type %s, not %s"
                                 % (name_argument,
                                    func.__qualname__,
                                    func.__code__.co_varnames[:func.__code__.co_argcount],
-                                   type_argument,
+                                   types_argument,
                                    type(kwargs[name_argument])))
 
         result = func(*args, **kwargs)
